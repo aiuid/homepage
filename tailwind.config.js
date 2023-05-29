3673df000cf0ba1626a8a26e996a9b57b1f44f29
Ian Connor <iconnor@projectlounge.com>
2023-05-29 13:52:33 +1000

    Style update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-gold': '#b88746',
+        'custom-yellow': '#fdf5a6'
+      }
+    },
+  },
+  plugins: [],
+}
